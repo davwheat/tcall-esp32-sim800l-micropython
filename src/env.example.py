a70a61e3dc85583363f2b7dccd955f9c5dcefb0d
David Wheatley <hi@davwheat.dev>
2021-06-30 19:42:09 +0100

    Add env file

---
+++ src/env.example.py
@@ -0,0 +1,7 @@
+def config():
+    return {
+        # Phone number to send messages to
+        "smsTarget": "+44123456789",
+        # URL to fetch JSON from
+        "smsFetchUrl": "https://gist.githubusercontent.com/davwheat/ae75c5f539e3aeb3bf2891d9c2509196/raw/f977026432ad3fe4d396e7225cfb5283ad0f3068/sms.json"
+    }
